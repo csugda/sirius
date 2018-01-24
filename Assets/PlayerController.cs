6c42118475bdac1da235c2286ba6137b576a0b9f
smilewood <milewood@gmail.com>
2018-01-24 13:26:37 -0700

    Player Left/Right controlls enabled

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D player;
+    public float moveForce, decelModifier, maxSpeed;
+	// Use this for initialization
+	void Start () {
+        player = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow) && player.velocity.x > -maxSpeed)
+            player.AddForce(new Vector2(-moveForce, 0));
+        else if (Input.GetKey(KeyCode.RightArrow) && player.velocity.x < maxSpeed)
+            player.AddForce(new Vector2(moveForce, 0));
+
+        if (player.velocity.x != 0)
+        {
+            player.velocity = (Vector2.Lerp(player.velocity, new Vector2(0, player.velocity.y), decelModifier)); 
+        }
+    }
+}
