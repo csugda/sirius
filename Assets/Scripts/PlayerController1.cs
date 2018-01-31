717ce18f8bba0828a7ed8e2e21fd3e449bb38ec6
smilewood <milewood@gmail.com>
2018-01-30 18:35:11 -0700

    Rework player controls to not use physics

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour {
+    private Transform player;
+    private BoxCollider2D playerCollider;
+    public  Vector3 velocity;
+    // Use this for initialization
+    void Start () {
+        player = this.gameObject.transform;
+        playerCollider = this.gameObject.GetComponent<BoxCollider2D>();
+        velocity = new Vector3(0, 0, 0);
+	}
+
+    public float speed;
+    private bool down = false;
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x = -speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x = speed;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
+
+        if (!down)
+        {
+            velocity.y -= 10 * Time.deltaTime;
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            velocity.y = 8;   
+        }
+
+        player.GetComponent<Rigidbody2D>().MovePosition(player.position + (velocity * Time.deltaTime));
+        
+        //player.SetPositionAndRotation(player.position + ( velocity*Time.deltaTime), player.rotation);
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        //Debug.Log("hit");
+        if (velocity.y < 0)
+        {
+            down = true;
+            
+        }
+        velocity.y = 0;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        down = false;
+    }
+}
