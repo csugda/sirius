65cdc44f2d3906743877d0110d269d2e3c4e3891
vicweeks <vicweeks@hotmail.com>
2018-02-07 18:18:00 -0700

    Added fluent-behaviour-tree library

---
+++ Assets/Scripts/BehaviorTree/SampleBehaviorTree.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FluentBehaviourTree;
+
+public class SampleBehaviorTree : MonoBehaviour {
+
+    IBehaviourTreeNode tree;
+
+	void Start () {
+        var builder = new BehaviourTreeBuilder();
+        this.tree = builder
+            .Sequence("Sample-Sequence")
+            .Do("SampleAction1", t =>
+            {
+                Debug.Log("I am doing action 1!");
+                return BehaviourTreeStatus.Success;
+            })
+            .Do("SampleAction2", tree =>
+            {
+                Debug.Log("I am doing the second action!");
+                return BehaviourTreeStatus.Success;
+            })
+            .End()
+            .Build();
+	}
+	
+	void Update () {
+        this.tree.Tick(new TimeData(100000));
+	}
+}
