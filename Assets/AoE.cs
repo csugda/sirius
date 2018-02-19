efc003c28e38d99ab0c5f3bb674752e472ed7b35
Jonathan Griego <31517248+JonathanGriego@users.noreply.github.com>
2018-02-19 16:57:12 -0700

    Added a few more assets

---
+++ Assets/AoE.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoE : MonoBehaviour
+{
+    // Apple of Exploding
+    //class variables
+    public float fuseTime; // time till detonation, can be set in the inspector
+
+}
