104265bde77ae039c8df3a53aad9c491706c2cec
Jonathan Griego <31517248+JonathanGriego@users.noreply.github.com>
2018-02-27 15:27:18 -0700

    interim commit

--- Assets/AttackDirection.cs
+++ Assets/AttackDirection.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class AttackDirection : MonoBehaviour {
     private SpriteRenderer attackBox;
-    //private Vector2 originalPos = new Vector3(GameObject.Find("Player").transform.position.x, GameObject.Find("Player").transform.position.y);
-    //private Vector2 flippedPos = new Vector3(-0.7f, 1.21f);
+
+    public float xAdjust = 0.7f;
+    public float yAdjust = 0.968f;
+
     private void Awake()
     {
         attackBox = GameObject.Find("PlayerSprite").GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update () {
         if (attackBox.flipX)
         {
-            transform.position = new Vector3((GameObject.Find("Player").transform.position.x - 0.7f), GameObject.Find("Player").transform.position.y + 0.968f);
+            transform.position = new Vector3((GameObject.Find("Player").transform.position.x - xAdjust), GameObject.Find("Player").transform.position.y + yAdjust);
         }
         else
         {
-            transform.position = new Vector3((GameObject.Find("Player").transform.position.x + 0.7f), GameObject.Find("Player").transform.position.y + 0.968f);
+            transform.position = new Vector3((GameObject.Find("Player").transform.position.x + xAdjust), GameObject.Find("Player").transform.position.y + yAdjust);
         }
 	}
 }
